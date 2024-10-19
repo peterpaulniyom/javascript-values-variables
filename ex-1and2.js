b5d1aae60c8312495c5ce65e169c9b69bbc4235b
Pitak Niyom <peterPaul@MacBook-Pro-khxng-Sutanya.local>
2024-10-19 12:29:22 +0700

    Peter: assigment sent

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let personName = 'Karter';
+let familyName = 'Anderson';
+let personAddress = '54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai';
+
+const restaurantName = "Chef's Table by Chef James";
+const totalAmount = 15000;
+const numRestaurants = 1500;
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+personAddress = '(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641';
+totalAmount = 5000;
+numRestaurants = 2500;
+
+
